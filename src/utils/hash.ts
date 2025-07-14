b5a1607f5f9078c5441b8e70078020533d38a780
Douglas Shinzato <shinzato.douglas@gmail.com>
2025-07-14 19:52:21 -0400

    forgot hash.ts on the last commit

---
+++ src/utils/hash.ts
@@ -0,0 +1,7 @@
+import { hash, compare } from "bcrypt";
+
+//Cria o hash da senha do usuário, utilizando o bcrypt.
+export const hashPassword = (password: string) => hash(password, 10);
+//Verifica se a senha digitada pelo o usuário para o login é a mesma salva no banco de dados para  respectivo usuário.
+export const verifyPassword = (password: string, hash: string) => compare(password, hash);
+//Não é necessario inferir o tipo de retorno das funções hashPassword e verifyPassword, pois o TypeScript já infere automaticamente o tipo de retorno baseado na assinatura das funções do pacote bcrypt.
