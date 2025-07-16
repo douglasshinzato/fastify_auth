c8965819555dfe9d8208062677d4d56a1565f6b7
Douglas Shinzato <shinzato.douglas@gmail.com>
2025-07-15 21:45:50 -0400

    changed "verifyPassword" to "comparePassword" on hashPassword.ts

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -2,13 +2,14 @@ import { prisma } from "src/lib/prisma";
 import { RegisterUserInput } from "src/schemas/userSchemas";
 import { hashPassword } from "src/utils/hashPassword";
 
-//RegisterUserInput é o tipo inferido do schema registerSchema
+// RegisterUserInput é o tipo inferido do schema registerSchema
 export async function registerNewUser(data: RegisterUserInput) {
-  //prisma.(tabela que irá ser acessada).findUnique({ where: { campo: valor } });
+  // prisma.(tabela que irá ser acessada).findUnique({ where: { campo: valor } });
   const existingUser = await prisma.user.findUnique({ where: { email: data.email } });
   if (existingUser) {
     throw new Error('Usuário já cadastrado com este e-mail');
   }
+  // Chama a função hashPassword para criptografar a senha do usuário
   const hashedPassword = await hashPassword(data.password)
 
   const newUser = await prisma.user.create({
--- src/utils/hashPassword.ts
+++ src/utils/hashPassword.ts
@@ -1,7 +1,9 @@
 import { hash, compare } from "bcrypt";
 
-//Cria o hash da senha do usuário, utilizando o bcrypt.
+// Cria o hash da senha do usuário, utilizando o bcrypt.
 export const hashPassword = (password: string) => hash(password, 10);
-//Verifica se a senha digitada pelo o usuário para o login é a mesma salva no banco de dados para  respectivo usuário.
-export const verifyPassword = (password: string, hashed: string) => compare(password, hashed);
-//Não é necessario inferir o tipo de retorno das funções hashPassword e verifyPassword, pois o TypeScript já infere automaticamente o tipo de retorno baseado na assinatura das funções do pacote bcrypt.
+// Verifica se a senha digitada pelo o usuário para o login é a mesma salva no banco de dados para  respectivo usuário.
+// O 10 significa o número de rounds de salting, que é um processo de segurança para tornar o hash mais resistente a ataques.
+// Salting é o processo de adicionar dados aleatórios a uma senha antes de aplicar o hash, para garantir que mesmo senhas iguais resultem em hashes diferentes.
+export const comparePassword = (password: string, hashed: string) => compare(password, hashed);
+// Não é necessario inferir o tipo de retorno das funções hashPassword e verifyPassword, pois o TypeScript já infere automaticamente o tipo de retorno baseado na assinatura das funções do pacote bcrypt.
